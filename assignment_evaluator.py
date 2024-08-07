4d15be7add409d93a7502927fecbf782f286a94c
pankajpansari <pankaj.pansari1@proton.me>
2024-08-08 00:39:58 +0530

    Improved progress display at console

--- assignment_evaluator.py
+++ assignment_evaluator.py
@@ -1,30 +1,47 @@
+import time
+from tqdm import tqdm
 import shutil
 from pathlib import Path
 from proposer_llm import ProposerLLM
 from aggregator_llm import AggregatorLLM
 from annotator_llm import AnnotatorLLM
-from config_loader import CONFIG
+from utils import CONFIG, parse_output
 
 def eval():
 
-    for parameter in CONFIG['parameters']:
-        for model in CONFIG['proposers']['models']:
-            print(f"Running Proposer for {parameter['name']} on {model['name']}\n")
+    total_params = len(CONFIG['parameters'])
+    total_models = len(CONFIG['proposers']['models'])
+
+    for param_index, parameter in enumerate(tqdm(CONFIG['parameters'])):
+        print(f"\nProcessing parameter ({param_index + 1}/{total_params}) : {parameter['name']}")
+
+        for model_index, model in enumerate(CONFIG['proposers']['models']):
+            if model_index == 0:
+                print(f"Running Proposer on {model['type']} ({model_index + 1}/{total_models})")
+            else:
+                print(f"                 on {model['type']} ({model_index + 1}/{total_models})")
             proposer = ProposerLLM(model, parameter)
             proposer.run()
 
-        print(f"Running Aggregator for {parameter['name']} on {CONFIG['aggregator']['model']['name']}\n")
+        print("Running Aggregator")
         aggregator = AggregatorLLM(CONFIG['aggregator']['model'], parameter)
         aggregator.run()
 
-        print(f"Running Annotator for {parameter['name']} on {CONFIG['annotator']['model']['name']}\n")
+        print("Running Annotator")
         annotator = AnnotatorLLM(CONFIG['annotator']['model'], parameter)
         annotator.run()
-        print(f"Done\n")
 
         src = Path(annotator.output_path) / Path(annotator.output_filename)
         dest = Path(CONFIG['assignment']['output_path']) / f"{CONFIG['assignment']['problem_file'].split('/')[1].split('.')[0]}_annotator_{CONFIG['annotator']['model']['type']}.txt"
         shutil.copyfile(src, dest)    
 
+        print(f"\nParameter {parameter['name']} completed ({param_index + 1}/{total_params})\n")
+
+    parse_output(CONFIG)
+    print("\nAll parameters processed. Evaluation complete.")
+
 if __name__ == '__main__':
+    start_time = time.time()
     eval()
+    end_time = time.time()
+    print(f"\nTotal execution time: {end_time - start_time:.2f} seconds")
